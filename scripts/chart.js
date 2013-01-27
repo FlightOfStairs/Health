de7e6d201e42b70c9b42fa36169c118c135f765a
Alistair Smith <alistairsmith@flightofstairs.org>
2013-01-27 15:08:33 +0000

    Refactoring html.

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,32 +1,34 @@
 $(function () {
-    var charts = ["weight", "bmi", "fat", "water"];
+	var charts = ["weight", "bmi", "fat", "water"];
 
-    var options = {
-        series : {
-            lines : { show: true },
-            points : { show: true }
-        },
-        grid : {
-            hoverable : true,
-            clickable : true
-        },
-        xaxis : {
-            mode : "time",
-            timeformat : "%y-%m-%d",
-            minTickSize : [1, "day"]
-        }
-    };
+	var options = {
+		series: {
+			lines: { show: true },
+			points: { show: true }
+		},
+		grid: {
+			hoverable: true,
+			clickable: true
+		},
+		xaxis: {
+			mode: "time",
+			timeformat: "%y-%m-%d",
+			minTickSize: [1, "day"]
+		}
+	};
 
-    var fillChart = function(chart, data, field) {
-        var series = $.map(data, function(entry) {
-            return [[new Date(entry.date), entry[field]]];
-        });
-        $.plot(chart, [series], options);
-    }
+	var fillChart = function (chart, data, field) {
+		var series = $.map(data, function (entry) {
+			return [
+				[new Date(entry.date), entry[field]]
+			];
+		});
+		$.plot(chart, [series], options);
+	};
 
-    $.getJSON('data.json', function(data) {
-        charts.forEach(function(chart) {
-            fillChart(document.getElementById(chart), data, chart);
-        });
-    })
+	$.getJSON('data.json', function (data) {
+		charts.forEach(function (chart) {
+			fillChart($('#' + chart + " div.chart-placeholder"), data, chart);
+		});
+	})
 });
